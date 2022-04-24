89f7d503becbfc1910750abe80f159ac30d2b59f
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-04-24 15:03:36 -0500

    default route home created

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,3 +1,18 @@
+import { useEffect } from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Home from './pages/Home/Home'
+
 export default function App() {
-  return <div className="App">hola</div>
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    navigate('/home')
+  }, [])
+
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/home" element={<Home />} />
+    </Routes>
+  )
 }
