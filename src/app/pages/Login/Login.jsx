f9efb243ad763c993934caecd3489715dc339d07
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-04-25 12:39:51 -0500

    Login styles update

--- src/app/pages/Login/Login.jsx
+++ src/app/pages/Login/Login.jsx
@@ -1,5 +1,7 @@
 import Menu from '../../common/Menu'
 import { Input, Box, Typography, Button } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { red } from '@mui/material/colors'
 export default function Login() {
   const styles = {
     BoxContainer: {
@@ -19,6 +21,25 @@ export default function Login() {
     Title: {
       textAlign: 'center',
     },
+    link: {
+      textDecoration: 'none',
+      color: '#1976D2',
+      cursor: 'pointer',
+      '&:hover': {
+        textDecoration: 'underline',
+      },
+    },
+    linkReactRouter: { textDecoration: 'none' },
+    LoginButtonFG: {
+      display: 'flex',
+      justifyContent: 'space-between',
+    },
+    LoginButtonGoogle: {
+      backgroundColor: red[500],
+      '&:hover': {
+        backgroundColor: red[600],
+      },
+    },
   }
 
   return (
@@ -31,9 +52,25 @@ export default function Login() {
           </Typography>
           <Input placeholder="email@email.com" type="email" />
           <Input placeholder="*********" type="password" />
+          <Link to="/register" style={styles.linkReactRouter}>
+            <Typography
+              variant="h6"
+              component="span"
+              gap="10px"
+              sx={styles.link}
+            >
+              Don't have an account?
+            </Typography>
+          </Link>
           <Button type="submit" variant="contained">
             Login
           </Button>
+          <Box sx={styles.LoginButtonFG}>
+            <Button variant="contained" sx={styles.LoginButtonGoogle}>
+              Google
+            </Button>
+            <Button variant="contained">Facebook</Button>
+          </Box>
         </Box>
       </Box>
     </main>
