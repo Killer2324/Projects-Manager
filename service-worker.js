ac135b3f5a12af8d48422131c11f16ee02a079c3
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-05-01 12:31:31 -0500

    gh-pages

--- service-worker.js
+++ service-worker.js
@@ -14,18 +14,18 @@
  * limitations under the License.
  */
 
-import { offlineFallback } from "workbox-recipes";
-import { setDefaultHandler } from "workbox-routing";
-import { NetworkOnly } from "workbox-strategies";
+import { offlineFallback } from 'workbox-recipes'
+import { setDefaultHandler } from 'workbox-routing'
+import { NetworkOnly } from 'workbox-strategies'
 
 // Asset hashes to see if content has changed.
-const assetHashes = self.__WB_MANIFEST;
-console.log(assetHashes);
+const assetHashes = self.__WB_MANIFEST
+console.log(assetHashes)
 
 // Sets a default Network Only handler, but consider writing your own handlers, too!
-setDefaultHandler(new NetworkOnly());
+setDefaultHandler(new NetworkOnly())
 
 // HTML to serve when the site is offline
 offlineFallback({
-  pageFallback: "/offline.html",
-});
+  pageFallback: '/offline.html',
+})
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from "vite";
-import { VitePWA as pwa } from "vite-plugin-pwa";
-import manifest from "./manifest.json";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import { VitePWA as pwa } from 'vite-plugin-pwa'
+import manifest from './manifest.json'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Projects-Manager/',
   plugins: [
     react(),
     pwa({
-      strategies: "injectManifest",
-      srcDir: "",
-      filename: "service-worker.js",
+      strategies: 'injectManifest',
+      srcDir: '',
+      filename: 'service-worker.js',
       manifest,
     }),
   ],
-});
+})
