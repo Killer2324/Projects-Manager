aa119511723b35d491f90bc4cd91e5e299bc8f5f
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-05-12 21:52:00 -0500

    npm to pnpm

--- src/app/firebase/firebase.js
+++ src/app/firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 const firebaseConfig = {
-  apiKey: 'AIzaSyDhIyetfkckH9WnHh4zs_TcCRcOHBr6pE4',
-  authDomain: 'projects-manager-f5f1d.firebaseapp.com',
-  projectId: 'projects-manager-f5f1d',
-  storageBucket: 'projects-manager-f5f1d.appspot.com',
-  messagingSenderId: '898530604698',
-  appId: '1:898530604698:web:c866a68071b4def37bd123',
+  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
+  authDomain: `${import.meta.env.VITE_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${import.meta.env.VITE_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${import.meta.env.VITE_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: `${import.meta.env.VITE_FIREBASE_APP_ID}`,
 }
 
 const app = initializeApp(firebaseConfig)
