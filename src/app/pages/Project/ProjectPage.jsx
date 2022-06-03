6f24ea4a02d31258f3362082036782c5743187d4
Gonzalo Cabada <cabadagonzalo17@gmail.com>
2022-06-02 19:16:29 -0500

    principal template of project page

--- src/app/pages/Project/ProjectPage.jsx
+++ src/app/pages/Project/ProjectPage.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { db } from '../../firebase/firebase'
 import { useParams } from 'react-router-dom'
 import { onSnapshot, collection } from 'firebase/firestore'
-import {Box, Typography} from '@mui/material'
+import {Box, Typography, Button} from '@mui/material'
 import Spinner from '../../common/Spinner.jsx'
 export default function ProjectPage() {
   const { id } = useParams()
@@ -27,19 +27,33 @@ export default function ProjectPage() {
       padding: '20px 50px',
     },
     Title: {
-      padding: '20px 0px',
+      padding: '15px 0px',
+    },
+    SubTitle: {
+      padding: '20px 10px'
     }
   }
   
-  console.log(project[0])
-
   return (
     <Box sx={styles.BoxMain}>
       {
         project[0]?.data ? (
           <>
-            <Typography component='h1' variant='h1' >{project[0]?.data.title}</Typography>
-            <Typography sx={styles.Title}>{project[0]?.data.description}</Typography>
+            <Typography component='h1' variant='h1' sx={styles.Title}>
+              {project[0]?.data.title}
+            </Typography>
+            <hr/>
+            <Typography component='h4' variant='h4' sx={styles.SubTitle}>
+              Tasks
+            </Typography>
+            <div style={{padding: '5px', marginLeft: '15px', border: '1px solid #eee', borderRadius: '10px', marginBottom: '15px'}}>
+              <input placeholder="add your task" style={{background: 'transparent', fontSize:"20px", padding: '10px', borderRadius: 'none', outline: 'none', border: 'none', color: '#333', width: '100%'}}/>
+            </div>
+            <div style={{marginLeft: '15px', display: 'flex', justifyContent: 'flex-end'}}>
+              <Button variant="contained" color="primary">
+                add
+              </Button>
+            </div>
           </>
         ) : (
           <Spinner />
